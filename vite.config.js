2e6c1ba659babc5604984bf1b87bfbff1a13df03
Matheus <1410mlg@gmail.com>
2025-06-03 12:48:03 -0300

    ajustes

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/PROJETO-TELA_FILMES/' : '/',
+  base: '/PROJETO-TELA_FILMES/',
   plugins: [react()]
 })
